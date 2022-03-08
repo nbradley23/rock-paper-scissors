62c6a3f90aaab4d6ad1937f29c320148058929d4
Nick Bradley <n.bradley23@yahoo.com>
2022-03-08 11:14:07 -0600

    Add function for best of 5 game of R/P/S

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //Score Keeping
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
 let combinedScore = `${playerScore} : ${computerScore}`
 
 //Random Computer R/P/S Selection
 function computerPlay() {
+    let rpsArray = ['rock', 'paper', 'scissors']
+    return rpsArray[Math.floor(Math.random() * 3)]
+}
+
+//Random Player R/P/S Selection
+function playerPlay() {
     let rpsArray = ['rock', 'paper', 'scissors']
     return rpsArray[Math.floor(Math.random() * 3)]
 }
@@ -19,17 +25,34 @@ function rpsRound(playerSelection, computerSelection) {
     } else {
         return playerScissors(computerSelection);
     }
-
-
-
 }
 
+//Plays game of R/P/S, best of five
+function game() {
+    while (playerScore < 5 && computerScore < 5) {
+        rpsRound(playerPlay(), computerPlay())
+
+    }
+
+    if (playerScore === 5) {
+        playerScore = 0;
+        computerScore = 0;
+        console.log('Player has won the game!!')
+    }
+
+    if (computerScore === 5) {
+        playerScore = 0;
+        computerScore = 0;
+        console.log('Computer has won the game!!')
+    }
+}
 
 //Function for when player chooses Rock
 function playerRock(computerSelection) {
     if (computerSelection === 'paper') {
         console.log(`Player chose: rock`);
         console.log(`Computer chose: ${computerSelection}`);
+        computerWin();
         return "You lose!"
     } else if (computerSelection === "rock") {
         console.log(`Player chose: rock`);
@@ -38,6 +61,7 @@ function playerRock(computerSelection) {
     } else {
         console.log(`Player chose: rock`);
         console.log(`Computer chose: ${computerSelection}`);
+        playerWin();
         return "You are the winner!"
     }
 }
@@ -48,6 +72,7 @@ function playerPaper(computerSelection) {
     if (computerSelection === 'rock') {
         console.log(`Player chose: paper`);
         console.log(`Computer chose: ${computerSelection}`);
+        playerWin();
         return "You are the winner!"
     } else if (computerSelection === 'paper') {
         console.log(`Player chose: paper`);
@@ -56,6 +81,7 @@ function playerPaper(computerSelection) {
     } else {
         console.log(`Player chose: paper`);
         console.log(`Computer chose: ${computerSelection}`);
+        computerWin();
         return "You lose!"
     }
 }
@@ -65,6 +91,7 @@ function playerScissors(computerSelection) {
     if (computerSelection === 'paper') {
         console.log(`Player chose: scissors`);
         console.log(`Computer chose: ${computerSelection}`);
+        playerWin();
         return "You are the winner!"
     } else if (computerSelection === 'scissors') {
         console.log(`Player chose: scissors`);
@@ -73,10 +100,22 @@ function playerScissors(computerSelection) {
     } else {
         console.log(`Player chose: scissors`);
         console.log(`Computer chose: ${computerSelection}`);
+        computerWin();
         return "You lose!"
     }
 }
 
+//When player wins, adds 1 to their score and prints the combined score
+function playerWin() {
+    ++playerScore
+    console.log(`${playerScore} : ${computerScore}`)
+}
+
+//When computer wins, adds 1 to its score and prints the combined score
+function computerWin() {
+    ++computerScore
+    console.log(`${playerScore} : ${computerScore}`)
+}
 
 
 
